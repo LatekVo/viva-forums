69ffba6bbd58a247866159c2e956500ba6bfb366
LatekVon <latkaignacy@gmail.com>
2022-05-29 22:47:41 +0200

    server.js: now fully functional serving index.html: minor bug fix server.js: implemented node-express for file serving

--- server/assets/main.js
+++ server/assets/main.js
@@ -13,7 +13,7 @@ function sendLogin() {
 
     // communicate using xmlhttp, everyone else seems to be using that
 
-    var xmlurl = "login.php&login=" + loginField + "&passwd=" + passwordField; // the resource our server is asked to serve, /board is the main page
+    var xmlurl = "login&login=" + loginField + "&passwd=" + passwordField; // the resource our server is asked to serve, /board is the main page
     let xmlhttp = new XMLHttpRequest();
 
     // add &user=xyz&password=enc_xyz, redirect to a php script, that will get a record from node.js server, that will get a record from a mongoDB
--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@
 
 var http = require('http');
 var events = require("events");
+var fs = require('fs');
+
+// we'll try express atop of what's already here, it may be useful
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/assets/'));
+
 var mongo = require('mongodb');
 
 var MongoClient = require('mongodb').MongoClient;
@@ -40,54 +48,22 @@ let server = http.createServer(function(request, response) {
             break;
         case 'GET':
             var body = '';
-            request.on('data', function (data) {
-                body += data;
-            });
-            request.on('end', function () {
-                try {
-                    let post = JSON.parse(body);
-                    parse(request, post);
-                    response.writeHead(200, {"Content-Type": "text/plain"});
-                    response.end();
-                    return;
-                } catch (err) {
-                    response.writeHead(500, {"Content-Type": "text/plain"});
-                    response.write("Bad Post Data. Error: ", err);
-                    response.end();
-                    return;
-                }
-            });
+            response.statusCode = 200;
+            response.write();
             break;
         default:
             console.log("bad request type: ", request.method)
     }
     console.log("response sent")
 });
-/* gonna try something different
-// close neccesary things before exiting
-var Handler = function() {
-    process.on('exit', () => {
-        MongoClient.close();
-        console.log("exiting");
-    });
-}
 
-console.log("trying to start mongodb...");
-const client = new MongoClient(mongourl);
-async function initDb() {
-    let response = await client.connect()
-        .catch((err) => {
-            console.error(err);
-        });
+// seems like express can also process xmlhttp.. requests pretty well, hence the (req, res)
+app.get('/', function(req, res) {
+    res.writeHead('200');
+    res.write(fs.readFileSync('assets/index.html', 'utf8'));
+    res.send();
+});
 
-    if (client.connected()) {
-        console.log("successfully started mongodb");
-    } else {
-        console.log("failed to start mongodb, exiting!");
-        process.exit(1);
-    }
-}
-initDb();
-*/
-server.listen(3000);
+//server.listen(3000);
+var expressServer = app.listen(3000);
 console.log("server started");
