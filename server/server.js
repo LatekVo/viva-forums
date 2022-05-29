3758e8f46269bf9c4277e86ea03b09e0c22c34a1
LatekVon <latkaignacy@gmail.com>
2022-05-30 01:31:10 +0200

    a

--- server/server.js
+++ server/server.js
@@ -3,65 +3,28 @@
 var http = require('http');
 var events = require("events");
 var fs = require('fs');
+var bodyParser = require('body-parser')
+
+// local database
+var db = require('./database')
 
 // we'll try express atop of what's already here, it may be useful
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/assets/'));
-
-var mongo = require('mongodb');
-
-var MongoClient = require('mongodb').MongoClient;
-var mongourl = "mongodb://localhost:27017/mydb";
-
-function parse(response, data) {
-    // all of the json data gets valiated and processed here.
-    // compose a rosponse here as well
-    console.log("received: ", data);
-    console.log("response: ", response);
-}
-
-let server = http.createServer(function(request, response) {
-    console.log("received a request");
-
-    switch(request.method) {
-        case 'POST':
-            var body = '';
-            request.on('data', function (data) {
-                body += data;
-            });
-            request.on('end', function () {
-                try {
-                    let post = JSON.parse(body);
-                    parse(request, post);
-                    response.writeHead(200, {"Content-Type": "text/plain"});
-                    response.end();
-                    return;
-                } catch (err) {
-                    response.writeHead(500, {"Content-Type": "text/plain"});
-                    response.write("Bad Post Data. Error: ", err);
-                    response.end();
-                    return;
-                }
-            });
-            break;
-        case 'GET':
-            var body = '';
-            response.statusCode = 200;
-            response.write();
-            break;
-        default:
-            console.log("bad request type: ", request.method)
-    }
-    console.log("response sent")
-});
+app.use(bodyParser.urlencoded({extended : true}));
 
 // seems like express can also process xmlhttp.. requests pretty well, hence the (req, res)
 app.get('/', function(req, res) {
     res.writeHead('200');
     res.write(fs.readFileSync('assets/index.html', 'utf8'));
     res.send();
+});
+
+app.post('/login', function(req, res) {
+    var {username, password} = req.body;
+    console.log(uname);
 });
 
 //server.listen(3000);
