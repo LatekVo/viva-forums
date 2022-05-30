d498941e1a77a7ee47af2bb8032678fab37047cd
LatekVon <latkaignacy@gmail.com>
2022-05-31 00:23:00 +0200

    package.json: added cookie-parser, register.html: added correct fields, server.js: didn't plagiarise the whole db system style.css: fixed input fields being id-specific

--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ var fs = require('fs');
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
 
-// local database
-var db = require('./database')
-
 // we'll try express atop of what's already here, it may be useful
 var express = require('express');
 var app = express();
@@ -18,10 +15,33 @@ app.use(express.static(__dirname + '/assets/'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(cookieParser());
 
-// seems like express can also process xmlhttp.. requests pretty well, hence the (req, res)
-app.get('/', function(req, res) {
+var dbpath = "./database";
+
+async function addRecord(newRecord) {
+
+    var dbData = await fs.promises.readFile(dbpath,{ encoding : 'utf8'});
+
+    var dataObject = JSON.parse(dbData);
+
+    dataObject.push(newRecord);
+
+    // Writing all records back to the file
+    await fs.promises.writeFile(
+        dbpath,
+        JSON.stringify(dataObject, null, 2)
+    )
+
+}
+
+app.get('/', function (req, res) {
     res.writeHead('200');
     res.write(fs.readFileSync('assets/index.html', 'utf8'));
+    res.send();
+});
+
+app.get('/register', function (req, res) {
+    res.writeHead('200');
+    res.write(fs.readFileSync('assets/register.html', 'utf8'));
     res.send();
 });
 
@@ -29,11 +49,12 @@ app.post('/login', function(req, res) {
     var {username, password, remember} = req.body;
     console.log(req.body);
 });
-app.post('/login', function(req, res) {
-    var {username, password, remember} = req.body;
+app.post('/register', async function (req, res) {
+    var {username, email, password, passwordRepeat} = req.body;
     console.log(req.body);
+
+    await addRecord({username, email, password});
 });
 
-//server.listen(3000);
 var server = app.listen(3000);
 console.log("server started");
