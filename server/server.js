b5a22d1178383b270a125a8f3bfd9c9e471ad562
LatekVon <latkaignacy@gmail.com>
2022-06-01 00:42:26 +0200

    renamed databases, added post database expanded server.js, specifics in next commit successful.html added for redirection to cookie parsing

--- server/server.js
+++ server/server.js
@@ -15,21 +15,55 @@ app.use(express.static(__dirname + '/assets/'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(cookieParser());
 
-var dbpath = "./database";
+// wow looks so much cleaner without semicolons
+let dbpath = "./db_database"
+let dbCache = undefined
+let initDb = async () => {
+    dbCache = JSON.parse(
+        await fs.promises.readFile(
+            dbpath,
+            { encoding : 'utf8'}
+        )
+    );
+}
+initDb();
+// databases for posts
+// user, id, timestamp, image-src, data
+let postpath = "./db_postbase"
+let postCache = undefined
+let initPostDb = async () => {
+    postCache = JSON.parse(
+        await fs.promises.readFile(
+            postpath,
+            { encoding : 'utf8'}
+        )
+    );
+}
+initPostDb();
+
+// handle caching
+// handle (is already verified to be valid if it can be found here), username
+let userCache = [];
 
 async function addRecord(newRecord) {
 
-    var dbData = await fs.promises.readFile(dbpath,{ encoding : 'utf8'});
+    var repeats = dbCache.filter(function(query) {
+        return query.email === newRecord.email || query.username === newRecord.username;
+    });
 
-    var dataObject = JSON.parse(dbData);
+    if (repeats.length != 0) {
+        return false;
+    }
 
-    dataObject.push(newRecord);
+    dbCache.push(newRecord);
 
     // Writing all records back to the file
     await fs.promises.writeFile(
         dbpath,
-        JSON.stringify(dataObject, null, 2)
+        JSON.stringify(dbCache, null, 2)
     )
+
+    return true;
 
 }
 
@@ -47,13 +81,45 @@ app.get('/register', function (req, res) {
 
 app.post('/login', function(req, res) {
     var {username, password, remember} = req.body;
+
+    var responseUrl = 'assets/successful.html/&';
+
+    var user = dbCache.filter(function(query) {
+        return query.username === newRecord.username;
+    });
+
+    if (user.length == 0) {
+        responseUrl += "error=username_incorrect"
+        res.writeHead('401');
+    } else
+    if (user.password != password) {
+        responseUrl += "error=password_incorrect"
+        res.writeHead('401');
+    } else {
+        // temporary handle for the login duration
+        var userHash = Math.random().toString(36).substring(2);
+
+        userCache += {userHash, username};
+        responseUrl += "hash=" + userHash;
+        res.writeHead('200');
+    }
+
+    res.write(fs.readFileSync(responseUrl, 'utf8'));
+    res.send();
+
     console.log(req.body);
 });
-app.post('/register', async function (req, res) {
+app.post('/register', async (req, res) => {
     var {username, email, password, passwordRepeat} = req.body;
-    console.log(req.body);
 
     await addRecord({username, email, password});
+});
+app.post('post', async (req, res) => {
+    // handle and date will be located in a hidden input field
+    var {handle, date, imgUrl, text} = req.body;
+    console.log(req.body);
+
+    await addPost({username, email, password});
 });
 
 var server = app.listen(3000);
